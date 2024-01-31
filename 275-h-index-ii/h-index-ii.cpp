7e8adf1fb3f9e3bc0acbe0d195c0e04faad8f5b2
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-01-31 22:14:59 +0530

    Time: 15 ms (51.66%) | Memory: 20.9 MB (11.69%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int low=0,high=citations.size()-1;
+        int mid;
+        int n=citations.size();
+        if(n==0)return 0;
+        int ans;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(citations[mid]>=n-mid ){ans=n-mid;
+                high=mid-1;}
+            else low=mid+1;
+        }
+        return ans;
+    }
+};
