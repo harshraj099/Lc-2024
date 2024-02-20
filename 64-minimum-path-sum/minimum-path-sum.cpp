df44799b1bcbf10130ee9cc5d61c7bfa95f28531
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-02-20 11:41:19 +0530

    Time: 7 ms (67.21%) | Memory: 12.5 MB (35.14%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -13,6 +13,24 @@ public:
         int m=grid.size();
         int n=grid[0].size();
          vector<vector<int>>dp(m,vector<int>(n,-1));
-   return f(m-1,n-1,dp,grid);
+//    return f(m-1,n-1,dp,grid);
+
+   // tabulation
+    dp[0][0]=grid[0][0];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+        int top=grid[i][j];
+        if(j-1>=0)top+=dp[i][j-1];
+        if(j-1<0)top+=1e5;
+        int left=grid[i][j];
+        if(i-1>=0)left+=dp[i-1][j];
+        if(i-1<0)left+=1e5;
+
+        if(i==0 && j==0)dp[i][j]=grid[0][0];
+        else dp[i][j]=min(left,top);
+        }
+    }
+        return dp[m-1][n-1];
+
     }
 };
