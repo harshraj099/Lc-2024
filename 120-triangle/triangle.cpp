a3f2822db51263b8a55be8620d7f3ee074f20fc4
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-02-20 15:32:05 +0530

    Time: 4 ms (76.33%) | Memory: 11.5 MB (16.59%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -12,6 +12,20 @@ public:
         int n=triangle.size();
         int m=triangle[n-1].size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(0,0,n,triangle,dp);
+    //   return  f(0,0,n,triangle,dp);
+
+      // tabulation
+      for(int j=0;j<n;j++){
+          dp[n-1][j]=triangle[n-1][j];
+      }
+      for(int i=n-2;i>=0;i--){
+          for(int j=i;j>=0;j--){
+              int down=triangle[i][j]+dp[i+1][j];
+             int diagonal=triangle[i][j]+dp[i+1][j+1];
+             dp[i][j]=min(down,diagonal);
+          }
+      }
+
+      return dp[0][0];
     }
 };
