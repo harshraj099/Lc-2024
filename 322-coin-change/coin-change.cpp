0ece9c3dfb77e0ee403b18b5f36b12b14dfcc679
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-03-25 15:03:50 +0530

    Time: 31 ms (56.68%) | Memory: 46.7 MB (31.73%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -13,9 +13,32 @@ public:
         return dp[idx][k]=min(take,not_take);
     }
     int coinChange(vector<int>& coins, int amount) {
-       vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
-       int ans= f(coins.size()-1,coins,amount,dp);
-       if(ans==1e9)return -1;
-       return ans;
+    //    vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
+    //    int ans= f(coins.size()-1,coins,amount,dp);
+    //    if(ans==1e9)return -1;
+    //    return ans;
+
+       // tabulation 
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,0));
+        for(int k=1;k<=amount;k++)
+     { 
+            if(k%coins[0]==0) dp[0][k]=k/coins[0];
+            else dp[0][k]=1e5;
+        }
+        
+        for(int i=1;i<coins.size();i++){
+            for(int sum=1;sum<=amount;sum++){
+                  int not_take=0+dp[i-1][sum];
+        int take=1e5;
+        if(coins[i]<=sum)take=1+dp[i][sum-coins[i]];
+
+       dp[i][sum]=min(take,not_take);
+            }
+        }
+      
+    //    if(ans==1e9)return -1;
+     int ans= dp[coins.size()-1][amount];
+     if(ans>=1e5)return -1;
+     return ans;
     }
 };
