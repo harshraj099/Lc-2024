3c777e6ae020f4cac1dbffee491f70033d37cf34
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-03-27 15:43:29 +0530

    Time: 45 ms (12.28%) | Memory: 25.9 MB (8.23%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int f(int idx1,int idx2,string &text1, string &text2,vector<vector<int>>&dp){
-        if(idx1==-1 || idx2==-1)return 0;
+        if(idx1<0 || idx2<0)return 0;
         if(dp[idx1][idx2]!=-1)return dp[idx1][idx2];
 
         // if matches on idx
-        int take=0;
-        if(text1[idx1]==text2[idx2]) take=1+f(idx1-1,idx2-1,text1,text2,dp);
+        // int take=0;
+        // if(text1[idx1]==text2[idx2]) take=1+f(idx1-1,idx2-1,text1,text2,dp);
+        //or
+       if(text1[idx1]==text2[idx2])  return 1+f(idx1-1,idx2-1,text1,text2,dp);
         
         // not matches
-        int nt=0;
-       if(text1[idx1]!=text2[idx2])  nt=0+max(f(idx1-1,idx2,text1,text2,dp),f(idx1,idx2-1,text1,text2,dp));
+    //     int nt=0;
+    //    if(text1[idx1]!=text2[idx2])  nt=0+max(f(idx1-1,idx2,text1,text2,dp),f(idx1,idx2-1,text1,text2,dp));
+       //or
+    return  dp[idx1][idx2]=max(f(idx1-1,idx2,text1,text2,dp),f(idx1,idx2-1,text1,text2,dp));
 
-        return dp[idx1][idx2]=take+nt;
+        // return dp[idx1][idx2]=take+nt;
     }
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
