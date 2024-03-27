d9611d94477f8cfffeb9deb383b4238477a0e665
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-03-28 00:32:23 +0530

    Time: 29 ms (39.13%) | Memory: 25.7 MB (49.67%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int f(int idx1,int idx2,string &text1, string &text2,vector<vector<int>>&dp){
-        if(idx1==0 || idx2==0)return 0;
+        if(idx1<0 || idx2<0)return 0;
         if(dp[idx1][idx2]!=-1)return dp[idx1][idx2];
 
         // if matches on idx
         // int take=0;
         // if(text1[idx1]==text2[idx2]) take=1+f(idx1-1,idx2-1,text1,text2,dp);
         //or
-       if(text1[idx1-1]==text2[idx2-1])  return 1+f(idx1-1,idx2-1,text1,text2,dp);
+       if(text1[idx1]==text2[idx2])  return 1+f(idx1-1,idx2-1,text1,text2,dp);
         
         // not matches
     //     int nt=0;
@@ -21,8 +21,25 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        // sifted index to right by 1
-        return f(n,m,text1,text2,dp);
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
+        // return f(n-1,m-1,text1,text2,dp);
+
+        // tabulation by shifted index
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<m;i++)dp[0][i]=0;
+        for(int i=0;i<n;i++)dp[i][0]=0;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                int take=0;
+                if(text1[i-1]==text2[j-1]) take=1+dp[i-1][j-1];
+
+                 int nt=0;
+       if(text1[i-1]!=text2[j-1])  nt=0+max(dp[i-1][j],dp[i][j-1]);
+
+                dp[i][j]=take+nt;
+            }
+        }
+        return dp[n][m];
     }
 };
