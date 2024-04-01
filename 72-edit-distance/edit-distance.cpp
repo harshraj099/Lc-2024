d8d0c77a582c06208d8b35da306e85bd2f612c2c
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-04-01 21:08:30 +0530

    Time: 10 ms (38.92%) | Memory: 11.8 MB (71.20%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -5,19 +5,21 @@ public:
         if(j<0)return i+1; 
 
         if(dp[i][j]!=-1)return dp[i][j];
-        // if matches on idx
-    //     int take=0;
-    //   if(s[i]==t[j])  take= f(i-1,j-1,s,t,dp)+f(i-1,j,s,t,dp);
+        //if matches on idx
+        int take=1e5;
+      if(s[i]==t[j])  take= f(i-1,j-1,s,t,dp);
     // or
-    if(s[i]==t[j])  return dp[i][j]=0+f(i-1,j-1,s,t,dp);
+    // if(s[i]==t[j])  return dp[i][j]=0+f(i-1,j-1,s,t,dp);
         
         // not matches
-    // int not_take =f(i-1,j,s,t,dp);
-    //     return dp[i][j]=max(take,not_take);
-    // or
-   return dp[i][j]= 1+min(f(i,j-1,s,t,dp),     //insertion
+    int not_take =1+min(f(i,j-1,s,t,dp),     //insertion
                           min (f(i-1,j,s,t,dp),    //deletion
                            f(i-1,j-1,s,t,dp)));  // replace
+        return dp[i][j]=min(take,not_take);
+    // or
+//    return dp[i][j]= 1+min(f(i,j-1,s,t,dp),     //insertion
+//                           min (f(i-1,j,s,t,dp),    //deletion
+//                            f(i-1,j-1,s,t,dp)));  // replace
     }
     int minDistance(string word1, string word2) {
                    int n=word1.size();
