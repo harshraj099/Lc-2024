ee33f5e39476f2c4d31af1103e039e7619536bf2
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-04-05 20:35:47 +0530

    Time: 27 ms (70.05%) | Memory: 12.3 MB (34.31%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -5,18 +5,37 @@ public:
         int mini=1e9;
         if(dp[i][j]!=-1)return dp[i][j];
         for(int ind=i;ind<=j;ind++){
+               //check cut in every possible point
             int cost=cuts[j+1]-cuts[i-1]+f(i,ind-1,cuts,dp)+f(ind+1,j,cuts,dp);
             mini=min(mini,cost);
         }
         return dp[i][j]=mini;
     }
     int minCost(int n, vector<int>& cuts) {
-        //check cut in every possible point
+             int p=cuts.size();
         cuts.push_back(n);
         cuts.insert(cuts.begin(),0);
         sort(cuts.begin(),cuts.end());
-        
-        vector<vector<int>>dp(cuts.size(),vector<int>(cuts.size(),-1));
-        return f(1,cuts.size()-2,cuts,dp);
+        // vector<vector<int>>dp(cuts.size(),vector<int>(cuts.size(),-1));
+        // return f(1,cuts.size()-2,cuts,dp);
+
+        //tabulation
+
+        vector<vector<int>>dp(p+2,vector<int>(p+2,0));
+        // base case
+            // no base case
+        for(int i=p;i>=1;i--){
+        for(int j=1;j<=p;j++){
+                 int mini=1e9;
+               if(i>j)continue;
+             for(int ind=i;ind<=j;ind++){
+               //check cut in every possible point
+            int cost=cuts[j+1]-cuts[i-1]+dp[i][ind-1]+dp[ind+1][j];
+            mini=min(mini,cost);
+        }
+          dp[i][j]=mini;
+        }
+        }
+        return dp[1][p];
     }
 };
