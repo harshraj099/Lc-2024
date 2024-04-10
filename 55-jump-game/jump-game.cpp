fa96c6c22241b67c9aa7f1abed8f8a255e79b955
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-04-11 02:14:38 +0530

    Time: 595 ms (10.38%) | Memory: 54 MB (6.37%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool f(int idx,vector<int>& nums,int end,vector<int>&dp){
         if(idx>=end)return true;
-        if(nums[idx]==0)return false;
+        // if(nums[idx]==0)return false;
         if(dp[idx]!=-1)return dp[idx];
         for(int sum=1;sum<=nums[idx];sum++){
             if(f(idx+sum,nums,end,dp))return dp[idx]=true;
