edc80603af3ebdec7f94782ca6fd5ede31b59110
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-04-13 11:53:35 +0530

    Time: 20 ms (23.25%) | Memory: 11.8 MB (68.21%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool present(string &st,vector<string>& wordDict){
+        for(auto it:wordDict)if(it==st)return true;
+        
+        return false;
+    }
+    bool f(int idx,string &s,vector<string>& wordDict,vector<int>&dp){
+        if(idx>=s.size())return true;
+        if(dp[idx]!=-1)return dp[idx];
+        string st="";
+        bool check=false;
+        for(int i=idx;i<s.size();i++){
+            st+=s[i];
+            if(present(st,wordDict) && f(i+1,s,wordDict,dp))check=true;
+        }
+        return dp[idx]=check;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int>dp(s.size(),-1);
+         return f(0,s,wordDict,dp);  
+    }
+};
