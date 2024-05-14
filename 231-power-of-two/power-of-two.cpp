55aa0e1ba90c160ffcc2c78037258c0f9d7aff74
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-05-14 21:42:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (36.31%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-       if(abs(n)<1)return false;
-       if(n<0)return false;
-       //double k=2;
-       int x =log2(n);
-       if((1<<x)==n)return true;
-       return false;
+    //    if(n<0)return false;
+    //    //double k=2;
+    //    int x =log2(n);
+    //    if((1<<x)==n)return true;
+    //    return false;
+    if(n<=0)return false;
+    long long ans=n&(n-1);
+    cout<<ans;
+    if(ans==0)return true;
+    return false;
     }
 };
