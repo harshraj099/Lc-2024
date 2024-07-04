63d353940bcfd7f1f40102f4b8d16612125fbe24
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-07-04 22:00:35 +0530

    Time: 83 ms (60.65%) | Memory: 47.1 MB (27.85%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,35 +1,42 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<pair<int,int>>adj[n+1];
-        for(int i=0;i<times.size();i++){
-            int u=times[i][0];
-            int v=times[i][1];
-            int wt=times[i][2];
-            adj[u].push_back({v,wt});
-        }
-        // vector<int>vis(n+1,0);
-        vector<int>dist(n+1,1e9);
-        dist[0]=-1;
+       
+       vector<pair<int,int>>adj[n+1];
+       for(auto it:times){
+        adj[it[0]].push_back({it[1],it[2]});
+       }
+       
+    //    vector<int>vis(n+1,0);
+    //    vis[0]=1;
+       vector<int>dist(n+1,1e9);
+
+        int maxi=0;
         queue<pair<int,int>>q;
-        q.push({k,0});
+
+        q.push({k,0});  // node , time 
+        // vis[k]=1;
         dist[k]=0;
-        int ct=0;
-        int maxi=INT_MIN;
-        while(!q.empty()){
-            int node=q.front().first;
+        dist[0]=0;
+        // int ct=0;
+        while (!q.empty()){
+            int node =q.front().first;
             int time=q.front().second;
+            maxi=max(maxi,time);
+            // ct++;
             q.pop();
-            // ct++;     
-            for(auto it:adj[node]){ 
-          if(time+it.second<dist[it.first])
-          {dist[it.first]=time+it.second;
-         q.push({it.first,it.second+time});}
-                    }
+            // cout<<maxi;
+            for(auto it:adj[node]){
+                // vis[it.first]=1;
+                if(time+it.second<dist[it.first]){
+                    dist[it.first]=time+it.second;
+                    q.push({it.first,dist[it.first]});
+                }
+            }
         }
-        // cout<<ct;
-    long long ans=*max_element(dist.begin(),dist.end());
+
+        int ans=*max_element(dist.begin(),dist.end());
         if(ans>=1e9)return -1;
-        else return ans;
+        return ans;
     }
 };
