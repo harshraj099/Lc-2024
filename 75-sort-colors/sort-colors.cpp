ca454b4b1eedd5363dcf5791455a5b23d7852a8b
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-07-27 16:06:35 +0530

    Time: 4 ms (31.10%) | Memory: 10.1 MB (12.49%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
+        // dutch national fllag algorithm
+        // left me 0 hogi , mid me 1  and right me 2
+
+        int left=0,mid=0,right=nums.size()-1;
+        while(mid<=right){
+            if(nums[mid]==0){
+                swap(nums[left],nums[mid]);
+                left++;
+                mid++;
+            }
+            else if(nums[mid]==1)mid++;
+
+            else {
+                swap(nums[mid],nums[right]);
+                right--;
+            }
+        }
     }
 };
