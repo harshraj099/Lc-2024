fe7ab5ba78e9baa7573eec11411da1f818b76920
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-07-30 10:06:20 +0530

    Time: 33 ms (13.26%) | Memory: 26.8 MB (36.95%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -7,9 +7,9 @@ public:
         stack<int>st;
         vector<int>ans(n,-1); 
         for(int i=2*n-1;i>=0;i--){
-        //    while(!st.empty() && st.top()<=nums[i%n])st.pop();
-            if(i<n){
            while(!st.empty() && st.top()<=nums[i%n])st.pop();
+            if(i<n){
+        //    while(!st.empty() && st.top()<=nums[i%n])st.pop();
                 if(!st.empty())ans[i%n]=st.top();
             }
 
