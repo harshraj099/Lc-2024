3496600d10e66e87f72358987672c89920bbfa69
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-07-30 10:05:58 +0530

    Time: 17 ms (87.67%) | Memory: 27.4 MB (17.77%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        // it is circular or we can consider it as 2*n size nums
+        //or traverse nums twice
+        int n=nums.size();
+        stack<int>st;
+        vector<int>ans(n,-1); 
+        for(int i=2*n-1;i>=0;i--){
+        //    while(!st.empty() && st.top()<=nums[i%n])st.pop();
+            if(i<n){
+           while(!st.empty() && st.top()<=nums[i%n])st.pop();
+                if(!st.empty())ans[i%n]=st.top();
+            }
+
+            st.push(nums[i%n]);
+        }
+        return ans;
+    }
+};
