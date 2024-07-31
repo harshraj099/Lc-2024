8f897943ca92e34cda9a1e5169cb9b3e0a3ee5f8
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-07-31 12:38:21 +0530

    Time: 140 ms (14.71%) | Memory: 32.9 MB (46.48%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -5,14 +5,11 @@ public:
         int n=s.size();
         stack<int>st;
         for(int i=0;i<n;i++){
-            if(st.empty())st.push(s[i]);
-            else {
-                if(st.top()=='b'&& s[i]=='a'){
+                if(!st.empty() && st.top()=='b'&& s[i]=='a'){
                     ct++;
                     st.pop();
                 }
                 else st.push(s[i]);
-            }
         }
        return ct;
     }
