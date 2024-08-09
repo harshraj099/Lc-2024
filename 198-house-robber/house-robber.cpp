46b29bfb3bcd4a6008d52202ccfa9c00851f2b89
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-08-09 18:42:07 +0530

    Time: 4 ms (31.57%) | Memory: 9.4 MB (46.74%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -10,10 +10,24 @@ public:
 	    int notpick=solve(idx-1,arr,dp);
 	    return dp[idx]=max(pick,notpick);
 	}
+    int f(int idx,vector<int>& arr,vector<int>&dp){
+	    if(idx>=arr.size())return 0;
+	    // if(idx==0)return arr[0];
+	    if(dp[idx]!=-1)return dp[idx];
+	    //take
+	    int pick=f(idx+2,arr,dp)+arr[idx];
+	    //not take
+	    int notpick=f(idx+1,arr,dp);
+	    return dp[idx]=max(pick,notpick);
+	}
     int rob(vector<int>& nums) {
         int n=nums.size();
            vector<int>dp(n,-1);
+	return f(0,nums,dp);
+
 	// return solve(n-1,nums,dp);
+
+    // tabulation method
     dp[0]=nums[0];
     int fs=INT_MIN,ss=INT_MIN;
     for(int i=1;i<n;i++){
