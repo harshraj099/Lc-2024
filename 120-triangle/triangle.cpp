5363c7f0f3582472ad454b2cb2d48ae73f8b9dc6
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-08-09 22:37:04 +0530

    Time: 8 ms (27.54%) | Memory: 11.7 MB (19.61%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -11,10 +11,29 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         int m=triangle[n-1].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
     //   return  f(0,0,n,triangle,dp);
 
       // tabulation
+        vector<vector<int>>dp(n,vector<int>(m,1e9));
+        dp[0][0]=triangle[0][0];
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=i;j++){
+            int down=1e9;
+            if(i-1>=0)down=triangle[i][j]+dp[i-1][j];
+            int diagonal=1e9;
+           if(i-1>=0 && j-1>=0)diagonal=triangle[i][j]+dp[i-1][j-1];
+
+             dp[i][j]=min(down,diagonal);
+            //  cout<<dp[i][j]<<endl;
+            }
+            // cout<<endl;
+        }
+int mini=1e9;
+for(int i=0;i<m;i++)mini=min(mini,dp[n-1][i]);
+return mini;
+
+
       for(int j=0;j<n;j++){
           dp[n-1][j]=triangle[n-1][j];
       }
