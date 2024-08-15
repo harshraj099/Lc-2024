3bf761df3637cbbe43463dd4d44018eb7f30b1cf
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-08-15 23:57:24 +0530

    Time: 47 ms (10.09%) | Memory: 43.7 MB (7.26%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -9,7 +9,7 @@ public:
         if(text1[i]==text2[j])take=1+f(i+1,j+1,n,m,text1,text2,dp);
         else not_take=max(f(i+1,j,n,m,text1,text2,dp),f(i,j+1,n,m,text1,text2,dp));
 
-        return  dp[i][j]=max(take,not_take);
+        return  dp[i][j]=take+not_take;
     }
     int longestCommonSubsequence(string text1, string text2) {
         long long n=text1.size();
