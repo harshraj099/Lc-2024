ecd4da89bc347a77fe4ea20a23cd242f56fa7ec3
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-08-15 23:59:10 +0530

    Time: 87 ms (5.02%) | Memory: 43.7 MB (7.26%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -5,9 +5,9 @@ public:
 
         if(dp[i][j]!=-1)return dp[i][j];
         // take
-        long long take=0,not_take=0;
+        long long take=0;
         if(text1[i]==text2[j])take=1+f(i+1,j+1,n,m,text1,text2,dp);
-        else not_take=max(f(i+1,j,n,m,text1,text2,dp),max(f(i,j+1,n,m,text1,text2,dp),f(i+1,j+1,n,m,text1,text2,dp)));
+        long long not_take=max(f(i+1,j,n,m,text1,text2,dp),max(f(i,j+1,n,m,text1,text2,dp),f(i+1,j+1,n,m,text1,text2,dp)));
 
         return  dp[i][j]=max(take,not_take);
     }
