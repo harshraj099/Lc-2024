5e8dd9ff662b0007720e105ec5f112ccfd8cc5f3
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-08-16 23:33:16 +0530

    Time: 66 ms (5.59%) | Memory: 30 MB (18.75%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-     int f(int idx1,int idx2,string &s, string &t,vector<vector<int>>&dp){
-        if(idx1<0 || idx2<0)return 0;
-        if(dp[idx1][idx2]!=-1)return dp[idx1][idx2];
+         int f(int i,int j,int n,string &text1, string &text2, vector<vector<int>>&dp){
+        if(i>=n || j<0)return 0;
 
-        // if matches on idx
-        int take=0;
-        if(s[idx1]==t[idx2]) take=1+f(idx1-1,idx2-1,s,t,dp);
-    //     or
-    //   if(text1[idx1]==text2[idx2])  return 1+f(idx1-1,idx2-1,text1,text2,dp);
-        
-        // not matches
-        int nt=0;
-        if(s[idx1]!=t[idx2])  nt=0+max(f(idx1-1,idx2,s,t,dp),f(idx1,idx2-1,s,t,dp));
-       //or
-//return  dp[idx1][idx2]=max(f(idx1-1,idx2,text1,text2,dp),f(idx1,idx2-1,text1,text2,dp));
+        if(dp[i][j]!=-1)return dp[i][j];
+        // take
+        int take=0,not_take=0;
+        if(text1[i]==text2[j])take=1+f(i+1,j-1,n,text1,text2,dp);
+        else not_take=max(f(i+1,j,n,text1,text2,dp),f(i,j-1,n,text1,text2,dp));
 
-        return dp[idx1][idx2]=take+nt;
+        return  dp[i][j]=max(take,not_take);
     }
     int minInsertions(string s) {
-         string temp=s;
-        reverse(s.begin(),s.end());
-        string t=s;
-        s=temp;
-
-          int n=s.size();
-        int m=t.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-      int r=  f(n-1,m-1,s,t,dp);
-      return s.size()-r;
+      int n=s.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+      return n- f(0,n-1,n,s,s,dp);
     }
 };
