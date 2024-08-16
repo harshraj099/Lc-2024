5583e1514e082da2627f28b722d56739ae9ec4a1
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-08-16 22:47:04 +0530

    Time: 70 ms (50.67%) | Memory: 74.7 MB (7.79%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-     int f(int i,int j,int n,int m,string &text1, string &text2, vector<vector<int>>&dp){
-        if(i>=n || j>=m)return 0;
+     int f(int i,int j,int n,string &text1, string &text2, vector<vector<int>>&dp){
+        if(i>=n || j<0)return 0;
 
         if(dp[i][j]!=-1)return dp[i][j];
         // take
         int take=0,not_take=0;
-        if(text1[i]==text2[j])take=1+f(i+1,j+1,n,m,text1,text2,dp);
-        else not_take=max(f(i+1,j,n,m,text1,text2,dp),f(i,j+1,n,m,text1,text2,dp));
+        if(text1[i]==text2[j])take=1+f(i+1,j-1,n,text1,text2,dp);
+        else not_take=max(f(i+1,j,n,text1,text2,dp),f(i,j-1,n,text1,text2,dp));
 
         return  dp[i][j]=max(take,not_take);
     }
     int longestPalindromeSubseq(string s) {
-        string temp=s;
-        reverse(s.begin(),s.end());
-        string t=s;
-        s=temp;
+        // string temp=s;
+        // reverse(s.begin(),s.end());
+        // string t=s;
+        // s=temp;
 
           int n=s.size();
-        int m=t.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-      return   f(0,0,n,m,s,t,dp);
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+      return   f(0,n-1,n,s,s,dp);
   
     }
 };
