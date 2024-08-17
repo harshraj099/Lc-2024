c2862f04b5d625dda84c7ca628e6f8134d2f3c70
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-08-17 15:12:47 +0530

    Time: 28 ms (26.95%) | Memory: 20.2 MB (53.73%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
-   	 int f(int i,int j,string &s, string &t,vector<vector<int>>&dp){
-        if(j<0)return 1;
-        if(i<0)return 0; // j<0 k baad hi ye check hoga 
+   	  int f(int i,int j,int n,int m,string &text1, string &text2, vector<vector<int>>&dp){
+        if(j>=m)return 1;
+        if(i>=n && j<m)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
+        // take
+        int take=0,not_take=0;
+        if(text1[i]==text2[j])take=f(i+1,j+1,n,m,text1,text2,dp)+f(i+1,j,n,m,text1,text2,dp);
+        else not_take=f(i+1,j,n,m,text1,text2,dp);
 
-        if(dp[i][j]!=-1)return dp[i][j];
-        // if matches on idx
-    //     int take=0;
-    //   if(s[i]==t[j])  take= f(i-1,j-1,s,t,dp)+f(i-1,j,s,t,dp);
-    // or
-    if(s[i]==t[j])  return dp[i][j]= f(i-1,j-1,s,t,dp)+f(i-1,j,s,t,dp);
-        
-        // not matches
-    // int not_take =f(i-1,j,s,t,dp);
-    //     return dp[i][j]=max(take,not_take);
-    // or
-   return dp[i][j]= f(i-1,j,s,t,dp);
+        return  dp[i][j]=take+not_take;
     }
     int numDistinct(string s, string t) {
                 int n=s.size();
         int m=t.size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,s,t,dp);
+        return f(0,0,n,m,s,t,dp);
+
+        //tabulation
+
+        // base case
+        //  int n=s.size();
+        // int m=t.size();
+        // vector<vector<double>>dp(n+1,vector<double>(m+1,0));
+        // //  if(j<=0)return 1;
+        // // if(i<=0)return 0;
+        // for(int i=0;i<=n;i++)dp[i][0]=1;
+        //  for(int j=1;j<=m;j++)dp[0][j]=0;
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        // if(s[i-1]==t[j-1]) dp[i][j]= dp[i-1][j-1]+dp[i-1][j];
+        //        else  dp[i][j]= dp[i-1][j];
+        //     }
+        // }
+        // // return (int)dp[n][m];
+        // //or
+        // return dp[n][m];
+
     }
 };
