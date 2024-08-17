119db5189fe2f4127596517d9ccb0b649f25a4cc
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-08-17 18:43:54 +0530

    Time: 27 ms (79.88%) | Memory: 31.8 MB (12.56%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -24,7 +24,29 @@ public:
     bool isMatch(string s, string p) {
         int n=s.size();
        int m=p.size();;
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(0,0,n,m,s,p,dp);
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
+        // return f(0,0,n,m,s,p,dp);
+
+         // tabulation
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        // base
+        dp[0][0]=1;
+        for(int j=1;j<=m;j++){
+       if(p[j-1]=='*')dp[0][j]=1;
+          else  break;   
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+           int take=0,not_take=0;
+        if(s[i-1]==p[j-1] | p[j-1]=='?')take=dp[i-1][j-1];
+        else if(p[j-1]=='*') not_take=dp[i-1][j]|dp[i][j-1];
+        // else dp[i][j]=0;
+
+         dp[i][j]=take|not_take;
+            }
+        }
+        return dp[n][m];
+
     }
 };
