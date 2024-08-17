7920668952bf858f51a47ae4af3fa8a34caced37
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-08-17 17:14:53 +0530

    Time: 7 ms (65.57%) | Memory: 12.9 MB (32.62%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -17,7 +17,26 @@ public:
     int minDistance(string word1, string word2) {
          int n=word1.size();
         int m=word2.size();;
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(0,0,n,m,word1,word2,dp);
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
+        // return f(0,0,n,m,word1,word2,dp);
+        
+        // tabulation
+        vector<vector<int>>dp(n+1,vector<int>(m+1,1e9));
+        // base
+        for(int i=0;i<=n;i++)dp[i][0]=i;
+        for(int j=0;j<=m;j++)dp[0][j]=j;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+           int take=1e9,state=1e9;
+        if(word1[i-1]==word2[j-1])take=dp[i-1][j-1];
+       else state=1+min(dp[i][j-1],//insert
+                        min(dp[i-1][j],//delete
+                        dp[i-1][j-1]));//replace
+
+         dp[i][j]=min(take,state);
+            }
+        }
+        return dp[n][m];
     }
 };
