962e3cac88fcedb58172f7a1872999274b63d86c
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-08-19 01:05:41 +0530

    Time: 64 ms (72.95%) | Memory: 13 MB (80.57%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -19,17 +19,29 @@ int f(int i,int n,vector<int>&nums,int prev,vector<vector<int>>&dp){
     //    vector<vector<int>>dp(n,vector<int>(n+1,-1));
     //    return f(0,n,nums,-1,dp);
     //    tabulation 1
-       vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        for(int i=n-1;i>=0;i--){
-            for(int prev=i-1;prev>=-1;prev--){
-               int take=0;
-                if(prev==-1 || nums[i]>nums[prev])take=1+dp[i+1][i+1];
-             // leave
-               int leave=dp[i+1][prev+1];
+    //    vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int prev=i-1;prev>=-1;prev--){
+    //            int take=0;
+    //             if(prev==-1 || nums[i]>nums[prev])take=1+dp[i+1][i+1];
+    //          // leave
+    //            int leave=dp[i+1][prev+1];
 
-               dp[i][prev+1]= max(take,leave);   
+    //            dp[i][prev+1]= max(take,leave);   
+    //         }
+    //     }
+    //     return dp[0][0];
+
+        // tabulation 2
+        vector<int>dp(n,1);
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+              if(nums[i]>nums[prev]) dp[i]=max(dp[i],1+dp[prev]);
             }
+            maxi=max(maxi,dp[i]);
         }
-        return dp[0][0];
+
+        return maxi;
     }
 };
