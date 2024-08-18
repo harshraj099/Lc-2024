dae978e0bfd2628477a211c0c0e32e9d20cdfb18
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-08-18 15:53:37 +0530

    Time: 477 ms (27.15%) | Memory: 224 MB (8.07%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,44 +1,18 @@
 class Solution {
-public:
-       // if fl=0 buy stock ,fl=1 sell before buying
-    int f(int i,vector<int>& prices,int n,  vector<vector<vector<int>>>&dp,int fl, int limit){
-        if(limit==2 || i==n)  return 0;
-        if(dp[i][fl][limit]!=-1)return dp[i][fl][limit];
-        // buy 
-        int profit=0;  //minimun to zero hi hoga logically
-      if(fl==0) profit=max(-prices[i]+f(i+1,prices,n,dp,!fl,limit),0+f(i+1,prices,n,dp,fl,limit));
-      // sell
-     if(fl) profit=max(prices[i]+f(i+1,prices,n,dp,!fl,limit+1),0+f(i+1,prices,n,dp,fl,limit));
+public:int f(int ind,int n,vector<int>& prices,vector< vector<vector<int>>>&dp,int fl,int bond){
+    if(ind==n || bond==2)return 0;
+    if(dp[ind][fl][bond]!=-1)return dp[ind][fl][bond];
+    // buy
+    int profit=0;
+    if(!fl)profit=max(-prices[ind]+f(ind+1,n,prices,dp,!fl,bond),f(ind+1,n,prices,dp,fl,bond));
+    // sell
+    if(fl)profit=max(prices[ind]+f(ind+1,n,prices,dp,!fl,bond+1),f(ind+1,n,prices,dp,fl,bond));
 
-      return  dp[i][fl][limit]=profit;
-    }
+     return  dp[ind][fl][bond]=profit;
+}
     int maxProfit(vector<int>& prices) {
-                // int n=prices.size();
-                // dp of n*2*3
-                //2-> buy or sell
-                // 3 limit at most 2 transction
-    //    vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-    //    return f(0,prices,n,dp,0,0);
-
-       // tabulation
          int n=prices.size();
-       vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        //basecase
-        // if(limit==2 || i==n)  return 0;
-        dp[n][0][2]=0;
-        dp[n][1][2]=0;
-        int profit=0;
-          for(int i=n-1;i>=0;i--){
-            for(int fl=0;fl<=1;fl++){
-                for(int cap=1;cap>=0;cap--){
-                    //buy
-    if(fl==0) profit=max(-prices[i]+dp[i+1][!fl][cap],0+dp[i+1][fl][cap]);
-      // sell
-     if(fl) profit=max(prices[i]+dp[i+1][!fl][cap+1],0+dp[i+1][fl][cap]);
-           dp[i][fl][cap]=profit;
-                }
-            }
-        }
-        return dp[0][0][0]; 
+       vector< vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
+       return f(0,n,prices,dp,0,0);
     }
 };
