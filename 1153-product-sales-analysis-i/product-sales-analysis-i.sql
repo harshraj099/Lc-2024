755b2f9dab758ebd733f77091ed343b3526631de
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-08-21 09:59:47 +0530

    Time: 1156 ms (69.17%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,2 +1,8 @@
 # Write your MySQL query statement below
-select prd.product_name,sal.year,sal.price from Product as prd right join sales as sal on prd.product_id=sal.product_id;
+-- select prd.product_name,sal.year,sal.price from Product as prd right join sales as sal on prd.product_id=sal.product_id;
+-- or
+-- select Product.product_name , Sales.year, Sales.price from Product , Sales 
+-- where Product.product_id=Sales.product_id ;
+-- or
+select Prd.product_name , Sal.year, Sal.price from Product as prd , Sales  as sal
+where Prd.product_id=Sal.product_id ;
