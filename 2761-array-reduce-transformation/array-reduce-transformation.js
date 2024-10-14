5fce52064bb7ff17251616aed92aa11dc4c0176a
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-10-14 20:47:57 +0530

    Time: 51 ms (73.21%) | Memory: 49.4 MB (27.69%) - LeetSync

--- 2761-array-reduce-transformation/array-reduce-transformation.js
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -4,11 +4,21 @@
  * @param {number} init
  * @return {number}
  */
-var reduce = function(nums, fn, init) {
-    let val=init;
-    for(let i=0;i<nums.length;i++){
-        val=fn(val,nums[i]);
-    }
 
-    return val;
+//  recurssion
+  function f(nums,fn,init,i){
+        if(i==nums.length)return init;
+
+        return f(nums,fn,fn(init,nums[i]),i+1);
+    }
+    
+var reduce = function(nums, fn, init) {
+    // let val=init;
+    // for(let i=0;i<nums.length;i++){
+    //     val=fn(val,nums[i]);
+    // }
+
+    // return val;
+
+    return f(nums,fn,init,0);
 };
