e72d6934a2eec9dc4e7b2ca338374ae73aa64cc7
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-10-14 22:08:51 +0530

    Time: 72 ms (12.53%) | Memory: 49.7 MB (42.34%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -3,19 +3,21 @@
  * @param {Promise} promise2
  * @return {Promise}
  */
-var addTwoPromises = async function(promise1, promise2) {
+// var addTwoPromises = async function(promise1, promise2) {
     
-//    return await promise1+ await promise2;
-// or
+// //    return await promise1+ await promise2;
+// // or
 
-    let [v1,v2]=await Promise.all([promise1,promise2]);
+//     let [v1,v2]=await Promise.all([promise1,promise2]);
+
+//     return v1+v2;
+// };
+
+    let addTwoPromises= async function(promise1,promise2){
+ let [v1,v2]=await Promise.all([promise1,promise2]);
 
     return v1+v2;
-};
-
-    // let addTwoPromises= async function(promise1,promise2){
-
-    // };
+    };
 
     // for my practice
     // addTwoPromises(promise.resolve(5),promise.resolve(6))
