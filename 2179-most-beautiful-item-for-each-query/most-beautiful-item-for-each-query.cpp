d920c95011e57154f15817135c087b206b8eeb94
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-11-13 00:50:25 +0530

    Time: 98 ms (9.84%) | Memory: 100.8 MB (26.69%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
         int n=items.size();
+        int mm=queries.size();
         sort(items.begin(), items.end());
         vector<int>ans;
         map<int,int>m;
-        int max_beauty = 0;
-
+       int max_beauty = 0;
         for(int i=0;i<n;i++){
-            max_beauty=max(max_beauty,items[i][1]);
-           m[items[i][0]]=max_beauty;
+            max_beauty = max(max_beauty, items[i][1]);
+            m[items[i][0]]=max_beauty;
         }
+  
 
-       for (int q : queries) {
-            auto it = m.upper_bound(q);
-            if (it == m.begin()) {
-                // No items with price <= q
-                ans.push_back(0);
-            } else {
-                // Get the maximum beauty for items with price <= q
-                --it;
-                ans.push_back(it->second);
-            }
+        for(int i=0;i<mm;i++){
+                auto it=m.upper_bound(queries[i]);
+                if(it==m.begin())ans.push_back(0);
+                else {
+                    --it;
+                     ans.push_back(it->second);
+                }
         }
 
         return ans;
