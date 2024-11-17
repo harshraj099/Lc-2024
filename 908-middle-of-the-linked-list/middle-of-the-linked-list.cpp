c194354406e97d16b410bdcae89edd4c3b30e7ae
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-11-17 10:47:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (69.76%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -10,36 +10,12 @@
  */
 class Solution {
 public:
-    // int length(ListNode* head,int &len){
-    //     if(head==NULL)return len;
-    //     len++;
-    //     length(head->next,len);
-    //     return len;
-    //         }
-
-    //         ListNode* f(ListNode* head,int l){
-    //             if(l==1) return head;
-    //             l--;
-    //            return f(head->next,l);
-    //             return head;
-    //         }
     ListNode* middleNode(ListNode* head) {
-    //     int len=0;
-    //    int l= length(head,len);
 
-    //    if(l%2==0){
-    //        l=l/2+1;
-    //        return f(head,l);
-    //    }
-    //    else{
-    //        l=(l+1)/2;
-    //        return f(head,l);
-    //    }
-
-    //  method 2
     // tortoise method
-    ListNode* slow=head,*fast=head;
-    while(fast!=NULL && fast->next!=NULL){
+   ListNode *slow=head,*fast=head;
+    
+    while(fast && fast->next){
         slow=slow->next;
         fast=fast->next->next;
     }
