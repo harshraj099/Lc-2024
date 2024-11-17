b51322da3e97da19afc075d134c2be452094155f
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-11-17 13:32:18 +0530

    Time: 0 ms (100.00%) | Memory: 116.9 MB (81.60%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,17 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+    ListNode* reverseList(ListNode* head){
+        ListNode *cur=head,*prev=NULL;
+
+        while(cur){
+            ListNode* temp=cur->next;
+          cur->next=prev;
+          prev=cur;
+          cur=temp;
+        }
+        // cout<<prev->val;
+
+        return prev;
+    }
     bool isPalindrome(ListNode* head) {
-        vector<int> listVals;
-        while (head) {
-            listVals.push_back(head->val);
-            head = head->next;
+         ListNode *fast=head,*slow=head;
+     while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
         }
+
+        ListNode *cur;
+       if(fast) cur=slow->next;
+       else cur=slow;
+
+     fast=reverseList(cur);
         
-        int left = 0, right = listVals.size() - 1;
-        while (left < right && listVals[left] == listVals[right]) {
-            left++;
-            right--;
+        while(head!=slow && fast){
+            // cout<<head->val<<" "<<fast->val<<endl;
+            if(head->val==fast->val){
+                head=head->next;
+                fast=fast->next;
+            // cout<<head->val<<" "<<fast->val<<endl;
+            }
+            else return 0;
         }
-        return left >= right;
+        return 1;
     }
 };
