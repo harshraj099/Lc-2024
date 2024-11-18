7ecb21ce5a33e6ee40692bea139ae0f77de80f32
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-11-18 13:25:54 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (34.80%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -10,54 +10,29 @@
  */
 class Solution {
 public:
-     void f(int ct,ListNode* head, int n){
-         if(ct==n-1){
-             ListNode* temp=head->next;
-             head->next=temp->next;
-             return;
-         }
-        return f(ct+1,head->next,n);
-     }
-     void ct(ListNode* head, int &cnt){
-         if(head==NULL)return;
-         cnt++;
-        return ct(head->next,cnt);
-     }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        // if(n==1){
-        //     head=head->next;
-        //     return head;
-        // }
-        // int count=0;
-        // ct(head,count);
-        // cout<<count;
-        // if(count==1)return NULL;
-        // int r=count-n+1;
-        // if(r==1){
-        //     head=head->next;
-        //     return head; 
-        // }
-        // f(1,head,r);
-        // return head;
+        int ct=1;
+        if(!head)return head;
+        if(!head->next)return head->next;
+         ListNode* slow=head,*fast=head;
 
-        if(head->next==NULL && n==1)return NULL;
-         ListNode* fast=head,*slow=head;
-           int ct=1;
-            while(ct<=n){
-               ct++;
-             fast=fast->next;
-           }
-           if(!fast)return head->next;
-           while(fast->next!=NULL){
-               fast=fast->next;
-               slow=slow->next;
-           }
-        // delete slow->next;
-    //    if(slow->next!=NULL)slow->next=slow->next->next;
-    // or
-      slow->next=slow->next->next;
+         while(fast && fast->next){
+            ct++;
+            slow=slow->next;
+            fast=fast->next->next;
+         }
+         int m;
+         if(fast)m=2*ct-1;
+         else m=(ct-1)*2;
 
-          return head;
-     
+         m=m-n;
+        slow=head;
+        if(m==0)return head->next;
+         while(--m){
+            slow=slow->next;
+         }
+         slow->next=slow->next->next;
+
+         return head;
     }
 };
