640b7d8613021f564500b0e06a09e599c0ab137a
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2024-12-18 22:33:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (9.96%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,27 +3,25 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         map<int,int>m;
         stack<int>st;
-            // cout<<"hi";
-        for(int i=0;i<nums2.size();i++){
-            if(st.empty()) st.push(nums2[i]);
-       else {
-        while(!st.empty() && nums2[i]>st.top()){
-            m[st.top()]=nums2[i];
-            st.pop();
+        int n=nums2.size();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty()){
+                if(st.top()>nums2[i]){
+                m[nums2[i]]=st.top();
+                break;
+                }
+                st.pop();
+            }
+            if(st.empty()){
+                m[nums2[i]]=-1;
+            }
+           st.push(nums2[i]); 
         }
-        st.push(nums2[i]);
-       }
+        vector<int> ans;
+        for(int i=0;i<nums1.size();i++){
+            ans.push_back(m[nums1[i]]);
         }
-        while(!st.empty()){
-            m[st.top()]=-1;
-            st.pop();
-        }
-        // cout<<"hi";
-         vector<int>ans;
-         for(int i=0;i<nums1.size();i++){
-         ans.push_back(m[nums1[i]]);
-         }
 
-         return ans;
+        return ans;
     }
 };
