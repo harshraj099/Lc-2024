2ae97c558070f75f2b70b43c1ce46365397a791e
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-02-11 01:24:51 +0530

    Time: 62 ms (13.79%) | Memory: 53.1 MB (13.04%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -19,8 +19,31 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-      if(f(0,n,coins,amount,dp)>=1e8)return -1;
-      return f(0,n,coins,amount,dp);
+    //     vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+    //   if(f(0,n,coins,amount,dp)>=1e8)return -1;
+    //   return f(0,n,coins,amount,dp);
+
+    //   tabulation
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,1e8));
+        // basecase
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+
+        for(int idx=1;idx<=n;idx++){
+            for(int target=0;target<=amount;target++){
+             // take
+            int take=1e8;
+        if(target>=coins[idx-1])take=1+dp[idx][target-coins[idx-1]];
+
+        // leave
+        int leave=dp[idx-1][target];
+
+          dp[idx][target]=min(take,leave);
+            }
+        }
+       
+       if(dp[n][amount]>=1e8)return -1;
+       return dp[n][amount];
     }
 };
