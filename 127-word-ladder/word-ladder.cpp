aa0a95b9b70a7d50fdf43619213260e1ff6c010d
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-04-03 00:37:05 +0530

    Time: 351 ms (28.16%) | Memory: 21.3 MB (46.75%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -14,7 +14,7 @@ public:
             int times=q.front().second;
             q.pop();
             if(st==endWord)return times;
-            for(int i=0;i<beginWord.size();i++){
+            for(int i=0;i<st.size();i++){
                 string temp=st;
               for(int j=0;j<26;j++){
                 temp[i]='a'+j;
