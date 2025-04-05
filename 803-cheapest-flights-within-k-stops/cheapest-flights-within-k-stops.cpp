fabbbc551f81857793dfcf417542e0f8b84c7589
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-04-05 19:35:04 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (58.62%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -3,33 +3,38 @@ public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<int>dist(n,1e9);
         vector<pair<int,int>>adj[n];
+        // priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> q;
+        queue<pair<int,pair<int,int>>>q;
+
         for(auto it:flights){
             int u=it[0];
             int v=it[1];
             int wt=it[2];
             adj[u].push_back({v,wt});
         }
-         queue<pair<int,pair<int,int>>>q;
-         q.push({0,{src,0}}); // dist,node
         dist[src]=0;
+        q.push({0,{src,k}});
         int mini=1e9;
         while(!q.empty()){
-            int len=q.front().first;
-            int from=q.front().second.first;
+            int wt=q.front().first;
+            int node=q.front().second.first;
             int ct=q.front().second.second;
-                q.pop();
-                if(from==dst){
-                    mini=min(mini,len);
-                }
-            for(auto it:adj[from]){
-                int to=it.first;
-                int new_len=len+it.second;
-                if(new_len<dist[to] && ct<=k){
-                    q.push({new_len,{to,ct+1}});
-                    dist[to]=new_len;
+            q.pop();
+            if(node==dst){
+                // return wt;
+                mini=min(mini,wt);
+            }
+            if(ct>=0){
+              for(auto it:adj[node]){
+                if(wt+it.second<dist[it.first]){
+                    dist[it.first]=wt+it.second;
+                    q.push({wt+it.second,{it.first,ct-1}});
                 }
             }
+            }
+            
         }
+
         if(mini!=1e9)return mini;
         return -1;
     }
