4e9449035dbcc7acf953013e05d456a526d8b2ae
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-04-10 19:02:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (34.79%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-vector<vector<int>>vec;
-   void getsubset(int idx,vector<int>& nums,vector<int>&v){
+    void f(int idx,int &sum,vector<int>& nums,int xr){
         if(idx==nums.size()){
-            vec.push_back(v);
+            sum+=xr;
             return;
         }
+
         // take
-        v.push_back(nums[idx]);
-        getsubset(idx+1,nums,v);
-        v.pop_back();
-        //not take
-       getsubset(idx+1,nums,v);
+        f(idx+1,sum,nums,xr^nums[idx]);
+
+        // leave
+        f(idx+1,sum,nums,xr);
+
     }
     int subsetXORSum(vector<int>& nums) {
-        vector<int>v;
-        getsubset(0,nums,v);
         int sum=0;
-        for(auto it:vec){
-            int xr=0;
-            for(auto i:it){
-                xr=xr^i;
-            }
-            sum+=xr;
-        }
+        f(0,sum,nums,0);
 
         return sum;
     }
