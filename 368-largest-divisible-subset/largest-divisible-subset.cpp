a13f6fa45e7d76ff7197e52ce5afceada2564b24
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-04-10 21:18:00 +0530

    Time: 6 ms (95.50%) | Memory: 12.5 MB (87.83%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,32 +1,53 @@
 class Solution {
 public:
+    void f(int idx,vector<int>& nums,vector<int>&res,vector<int>& temp){
+        if(idx==nums.size()){
+            if(temp.size()>res.size()){
+                res=temp;
+            }
+            return;
+        }
+
+        // take
+        if(temp.empty() || nums[idx]%temp.back()==0){
+            temp.push_back(nums[idx]);
+            f(idx+1,nums,res,temp);
+            temp.pop_back();
+        }
+
+        // leave
+        f(idx+1,nums,res,temp);
+    }
     vector<int> largestDivisibleSubset(vector<int>& nums) {
+
         int n=nums.size();
-        int maxi=0;
+        if(n==1)return nums;
         sort(nums.begin(),nums.end());
-        vector<int> hash(n);
-        vector<int>dp(n,1);
-        int idx=0;
-        for(int i=0;i<n;i++){
-                hash[i]=i;
-                for(int prev=0;prev<i;prev++){
-                    if(nums[i]%nums[prev]==0 && dp[i]<dp[prev]+1){
-                        dp[i]=dp[prev]+1;
-                        hash[i]=prev;
-                    }
+      
+      vector<int>res,temp;
+    //   f(0,nums,res,temp);
+         vector<int> dp(n, 1), prev(n, -1);
+        int max_idx = 0, max_size = 1;
+
+        // Step 3: Populate the DP arrays
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
                 }
-                if(dp[i]>maxi){
-                    maxi=dp[i];
-                    idx=i;
-                }
+            }
+            if (dp[i] > max_size) {
+                max_size = dp[i];
+                max_idx = i;
+            }
         }
-        vector<int>ans;
-        while(hash[idx]!=idx){
-            ans.push_back(nums[idx]);
-            idx=hash[idx];
-        }
-            ans.push_back(nums[idx]);
-            reverse(ans.begin(),ans.end());
-            return ans;
+
+        // Step 4: Reconstruct the largest divisible subse
+        for (int i = max_idx; i >= 0; i = prev[i]) {
+            res.push_back(nums[i]);
+            if (prev[i] == -1) break;
+        }      
+        return res;
     }
 };
