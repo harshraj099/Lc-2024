e44604c402f29d40d0d88296fda47ca2a241b203
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-04-12 01:18:48 +0530

    Time: 357 ms (81.08%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,7 +1,11 @@
 # Write your MySQL query statement below
 select case 
-    when id%2=1 and id+1 in (select id from Seat) then id+1
+    when id%2=1 and id+1 in (select id from Seat) then id+1 
+    -- means when id is odd and id+1 exist in "id" then select id+1 ie next id
+    -- since we wanna swap in this case 
     when id%2=0 then id-1
+    -- if id is even ie we wanna give it to id -1 person 
     else id
+    -- if none case exist siply return same id
     end as id,student
  from Seat order by id
