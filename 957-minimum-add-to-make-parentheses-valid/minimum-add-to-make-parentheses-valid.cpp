9d4cbc19c1c76cc1c8204b2fe9ce640c3b10152f
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-04-18 01:18:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (56.03%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        for(auto it:s){
+            if(!st.empty() && (st.top()=='(' && it==')')){
+        st.pop();
+        }
+        else {
+            st.push(it);
+        }
+        }
+
+        return st.size();
+    }
+};
