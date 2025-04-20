0376c6af302e692f8ac7d80be55a1d7f8682cfac
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-04-21 00:20:55 +0530

    Time: 12 ms (42.33%) | Memory: 12.7 MB (41.90%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,53 +1,30 @@
 class Solution {
 public:
-    void f(int idx,vector<int>& nums,vector<int>&res,vector<int>& temp){
-        if(idx==nums.size()){
-            if(temp.size()>res.size()){
-                res=temp;
-            }
-            return;
-        }
-
-        // take
-        if(temp.empty() || nums[idx]%temp.back()==0){
-            temp.push_back(nums[idx]);
-            f(idx+1,nums,res,temp);
-            temp.pop_back();
-        }
-
-        // leave
-        f(idx+1,nums,res,temp);
-    }
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-
         int n=nums.size();
-        if(n==1)return nums;
         sort(nums.begin(),nums.end());
-      
-      vector<int>res,temp;
-    //   f(0,nums,res,temp);
-         vector<int> dp(n, 1), prev(n, -1);
-        int max_idx = 0, max_size = 1;
-
-        // Step 3: Populate the DP arrays
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]) {
-                    dp[i] = dp[j] + 1;
-                    prev[i] = j;
+        int maxi=0,lastIdx;
+        vector<int>dp(n,1);
+        vector<int>hash(n);
+        for(int i=0;i<n;i++){
+            hash[i]=i;
+            for(int j=0;j<i;j++){
+                if(nums[i]%nums[j]==0 && dp[i]<dp[j]+1){
+                    dp[i]=dp[j]+1;
+                    hash[i]=j;
                 }
             }
-            if (dp[i] > max_size) {
-                max_size = dp[i];
-                max_idx = i;
+            if(dp[i]>maxi){
+                 maxi=dp[i];
+                 lastIdx=i;
             }
         }
-
-        // Step 4: Reconstruct the largest divisible subse
-        for (int i = max_idx; i >= 0; i = prev[i]) {
-            res.push_back(nums[i]);
-            if (prev[i] == -1) break;
-        }      
-        return res;
+       vector<int>ans;
+        while(hash[lastIdx]!=lastIdx){
+            ans.push_back(nums[lastIdx]);
+            lastIdx=hash[lastIdx];
+        }
+        ans.push_back(nums[lastIdx]);
+        return ans;
     }
 };
