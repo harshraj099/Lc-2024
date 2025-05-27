c9240dbb16f011d9eb226c97d29fc3975f05fd63
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-05-28 00:51:32 +0530

    Time: 205 ms (5.05%) | Memory: 110.6 MB (6.69%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> cnt(n + 1, 0);
-        cnt[0] = 1;
-        int ans = 0, t = 0;
-        for (int v : nums) {
-            t += v & 1;
-            if (t - k >= 0) {
-                ans += cnt[t - k];
-            }
-            cnt[t]++;
+
+        // correct but tle
+
+        //  int n=nums.size();
+        // vector<int>prefix(n+1,0);
+        // for(int i=0;i<n;i++){
+        //     prefix[i+1]=prefix[i]+((nums[i]%2==1)?1:0);
+        // }
+        // int ct=0;
+        // for(int i=0;i<n;i++){
+        //     int l=0;
+        //     while(l<=i){
+        //         if(prefix[i+1]-prefix[l]==k){
+        //             ct++;
+        //         }
+        //         l++;
+        //     }
+        // }
+        // return ct;
+
+        map<int,int>m;
+        int odd=0;
+        int res=0;
+        m[0]=1; //ek to when odd==k wala case k liye
+        for(auto it:nums){
+             odd+=(it%2);
+            res+=m[odd-k];
+            m[odd]++;
         }
-        return ans;
+
+        return res;
     }
 };
