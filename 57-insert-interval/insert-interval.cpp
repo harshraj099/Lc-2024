22789ac00ef5f37e771c3b29542ad297f2e477d1
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-05-31 01:02:07 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (93.85%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-           if(intervals.size()==0)return {newInterval};
+        vector<vector<int>> res;
+        int i=0;
+        int n=intervals.size();
+        while(i<n && intervals[i][1]<newInterval[0]){
+            res.push_back(intervals[i]);
+            i++;
+        }
 
-            vector<vector<int>>res;
-            vector<int>idx={0,0};
-            for(auto it:intervals){
-                if(it[1]<newInterval[0]){
-                    res.push_back({it[0],it[1]});
-                }
-                else if(it[1]>=newInterval[0]){
+        while(i<n && newInterval[1]>=intervals[i][0]){
+            newInterval[0]=min(newInterval[0],intervals[i][0]);
+             newInterval[1]=max(newInterval[1],intervals[i][1]);
+             i++;
+        }
 
-                    if(idx[0]==0 && idx[1]==0){
-                    if(it[0]<=newInterval[0]){
-                        idx={it[0],max(it[1],newInterval[1])};
-                    }
-                    else {
-                    if(it[0]<=newInterval[1]) idx={newInterval[0],max(it[1],newInterval[1])};
-                    else {
-                         res.push_back({newInterval[0],newInterval[1]});
-                          idx={it[0],it[1]};
-                          }
-                    }
-                    }
+        res.push_back(newInterval);
 
-                    else{
-                      if(it[0]<=idx[1]){
-                        idx={idx[0],max(it[1],idx[1])};
-                    }
-                  else {
-                    res.push_back({idx[0],idx[1]});
-                    idx={it[0],it[1]};
-                  }
-                    }
-                }
-            }
-            if(idx[0]==0 && idx[1]==0)  res.push_back({newInterval[0],newInterval[1]});
-         else res.push_back({idx[0],idx[1]});
-            return res;
+         while(i<n){
+           res.push_back(intervals[i]);
+            i++;
+        }
+
+        return res;
     }
 };
