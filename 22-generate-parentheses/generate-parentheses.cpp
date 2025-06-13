a59026a9839d11aa05df8af6bd177cd832ecf3e9
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-06-14 00:38:42 +0530

    Time: 1 ms (86.89%) | Memory: 15.5 MB (64.72%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    void f(int l,int r,vector<string>&ans,string s){
-        if(l==0 && r==0){
-            ans.push_back(s);
-            return;
-        }
-        // l
-       if(l>0){
-        //         s+='(';
-        // f(l-1,r,ans,s);
-        // s.pop_back();
-        // or
-        f(l-1,r,ans,s+"(");
-        }
 
-        //r
-
-       if(r>l){
-        //         s+=')';
-        // f(l,r-1,ans,s); 
-        // s.pop_back();
-        // or
-         f(l,r-1,ans,s+")");
-        } 
+     void solve(int i,int n,vector<string>&res,string s,int ct1,int ct2){
+        if(i==n){
+            res.push_back(s);
+            return ;
+        }
+        
+        // take '('
+        if(ct1<n/2)solve(i+1,n,res,s+'(',ct1+1,ct2);
+        
+        // take ')'
+         if(ct1>ct2 && ct2<n/2) solve(i+1,n,res,s+')',ct1,ct2+1);
     }
     vector<string> generateParenthesis(int n) {
-        vector<string>ans;
-         string s="";
-        f(n,n,ans,s);
-        return ans;
+          vector<string>res;
+        string s="";
+        solve(0,2*n,res,s,0,0);
+        
+        return res;
     }
 };
