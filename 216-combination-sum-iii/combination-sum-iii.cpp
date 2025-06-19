d5cd99750acdf17f45a45fa9db6375039fbbfbf0
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-06-20 00:19:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (60.11%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,29 +1,25 @@
 class Solution {
-public:
-    void f(int idx,vector<int>&temp,vector<int>&v,vector<vector<int>>&ans,int target,int n){
-       if(target==0 && n>0) return;
-        if(idx<0 && target>0)return;
-        if(n==0 ){
-         if(target==0)ans.push_back(v);
-            return ;
+public: 
+    void f(int i,vector<int>& digits, int target,int n, vector<int>&v,vector<vector<int>>&res,int k){
+        if(target==0 && k==0){
+             res.push_back(v);
+            return;
         }
         
-        // take 
-        if(target>=temp[idx]){
-        v.push_back(temp[idx]);
-        f(idx-1,temp,v,ans,target-temp[idx],n-1);
-        v.pop_back();
-}
-        // not take
-        f(idx-1,temp,v,ans,target,n);
+        for(int idx=i;idx<n;idx++){
+            if(idx!=i && digits[idx]==digits[idx-1])continue;
+            if(target-digits[idx]>=0){
+            v.push_back(digits[idx]);
+            f(idx+1,digits,target-digits[idx],n,v,res,k-1);
+            v.pop_back();
+            }
+        } 
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        if(n<=k || n>45 || n>=k*9)return {};
-        vector<vector<int>>ans;
+        vector<int>digits={1,2,3,4,5,6,7,8,9};
+        vector<vector<int>>res;
         vector<int>v;
-        vector<int>temp={1,2,3,4,5,6,7,8,9};
-        f(8,temp,v,ans,n,k);
-
-        return ans;
+        f(0,digits,n,9,v,res,k);
+        return res;
     }
 };
