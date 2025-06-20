64a29270f1e696799ae0df3991dc09d01427a409
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-06-20 23:47:20 +0530

    Time: 121 ms (16.82%) | Memory: 144.9 MB (12.32%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,59 +1,36 @@
 class Solution {
-public:
-// bool ispalindrome(string &s,int start,int end){
-//     while(start<=end){
-//         if(s[start]!=s[end])return false;
-//         start++;
-//         end--;
-//     }
-//     return true;
-// }
-//         void solve(int idx,int n,vector<vector<string>>&str,string &s,vector<string>&v){
-//             if(idx==n){
-//                     str.push_back(v);
-//                 return ;
-//             }
-//             for(int i=idx;i<n;i++){
-//         if (ispalindrome(s,idx,i)){ 
-//             string p="";
-//             for(int j=idx;j<=i;j++) p+=s[j];
-//             v.push_back(p);
-        
-//             solve(i+1,n,str,s,v);
-//             v.pop_back();
-//         }
-//             }
-//         }
-
-bool ispalindrome(string &s,int start,int end){
-    while(start<=end){
-        if(s[start]!=s[end])return false;
-        start++;
-        end--;
-    }
-    return true;
-}
-          void solve(int idx,int n,vector<vector<string>>&str,string &s,vector<string>&v){
-            if(idx==n){
-                    str.push_back(v);
-                return ;
+public: 
+      bool  ispalindrome(string s){
+            int i=0,r=s.size()-1;
+            while(i<r){
+                if(s[i]!=s[r])return false;
+                i++;
+                r--;
             }
-
-     for(int i=idx;i<n;i++){
-        if(ispalindrome(s,idx,i)){
-            string temp="";
-            for(int j=idx;j<=i;j++)temp+=s[j];
-            v.push_back(temp);
-            solve(i+1,n,str,s,v);
-            v.pop_back();
+            return true;
         }
-     }
+      void f(int i,string s,int n, vector<string> v,vector<vector<string>>&res){
+        if(i==n){
+             res.push_back(v);
+            return;
+        }
+        
+        for(int idx=i;idx<n;idx++){
+            string st = s.substr(i, idx - i + 1);
+            if(ispalindrome(st)){
+            v.push_back(st);
+            f(idx+1,s,n,v,res);
+            v.pop_back();
+            }
+        }
 
-        }  
+        
+    }
     vector<vector<string>> partition(string s) {
-        vector<string>v;
-        vector<vector<string>>str;
-        solve(0,s.size(),str,s,v);
-        return str;
+         int n=s.size();
+        vector<vector<string>>res;
+        vector<string> v;
+        f(0,s,n,v,res);
+        return res;
     }
 };
