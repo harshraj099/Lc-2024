57999cc06ea700f709f55af37ca5e8ad9c002be0
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-07-15 00:30:12 +0530

    Time: 41 ms (14.75%) | Memory: 13.7 MB (89.42%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
-    int f(int i,int j,vector<int>& cuts,vector<vector<int>>&dp){
-        if(i>j)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int mini=1e9;
-        for(int idx=i;idx<=j;idx++){
-            int sum=f(i,idx-1,cuts,dp)+f(idx+1,j,cuts,dp)+cuts[j+1]-cuts[i-1];
-            mini=min(mini,sum);
-        }
-
-        return dp[i][j]=mini;
-    }
-    int minCost(int n, vector<int>& cuts) {
-            int m=cuts.size();
-        cuts.insert(cuts.begin(),0);
-        cuts.push_back(n);
-        // vector<vector<int>>dp(m+2,vector<int>(m+2,-1));
-        sort(cuts.begin(),cuts.end());
-        // return f(1,cuts.size()-2,cuts,dp);
-        // tabulation
-        vector<vector<int>>dp(m+2,vector<int>(m+2,0));
-        for(int i=m;i>=1;i--){
-            for(int j=i;j<m+1;j++){
-             int mini=1e9;
-        for(int idx=i;idx<=j;idx++){
-            int sum=dp[i][idx-1]+dp[idx+1][j]+cuts[j+1]-cuts[i-1];
-            mini=min(mini,sum);
-        }
-         dp[i][j]=mini;   
+        int f(int l,int r,vector<int>& cuts,int i,int j,vector<vector<int>>&dp){
+            if(i>j)return 0;
+            if(dp[i][j]!=-1)return dp[i][j];
+            int mini=1e9;
+            for(int k=i;k<=j;k++){
+            int cut=(r-l)+f(l,cuts[k],cuts,i,k-1,dp) + f(cuts[k],r,cuts,k+1,j,dp); 
+            mini=min(cut,mini);    
             }
+            return dp[i][j]=mini;
         }
-        return dp[1][m];
+    int minCost(int N, vector<int>& cuts) {
+        sort(cuts.begin(),cuts.end());
+        int n=cuts.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return f(0,N,cuts,0,cuts.size()-1,dp);
     }
 };
