fbe3ec1c1895bb29df22fbadd4eae6584b29f1fd
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-07-20 17:20:36 +0530

    Time: 7 ms (32.94%) | Memory: 18.2 MB (59.60%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>res;
-	 queue<int>q;
-	 vector<int>indegree(numCourses,0);
-     vector<int>adj[numCourses];
-	 for(int i=0;i<prerequisites.size();i++){
-	 int u=prerequisites[i][0];
-	 int v=prerequisites[i][1];
-     adj[v].push_back(u);
-     indegree[u]++;
-	 }
-     int ct=0;
-	 for(int i=0;i<numCourses;i++){
-	     if(indegree[i]==0)q.push(i);
-	 }
-	 
-	 while(!q.empty()){
-	     int node=q.front();
-	     q.pop();
-	     res.push_back(node);
-         ct++;
-	     for(auto it:adj[node]){
-	         indegree[it]--;
-	         if(indegree[it]==0){
-	             q.push(it);
-	         }
-	     }
-	 }
-	 if(ct==numCourses)return res;
-     return {};
+        vector<int>adj[numCourses];
+        vector<int>indeg(numCourses,0);
+        for(auto it: prerequisites){
+            int u=it[0],v=it[1];
+            adj[v].push_back(u);
+            indeg[u]++;
+        }
+
+        queue<int>q;
+        for(int i=0;i<numCourses;i++)
+        if(indeg[i]==0)q.push(i);
+
+        vector<int>ans;
+        
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto it:adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0)q.push(it);
+            }
+        }
+        if(ans.size()!=numCourses)return {};
+        return ans;
     }
 };
