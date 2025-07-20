57e9eb7676630657152f5334cc5750a1f329bc5f
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-07-20 13:53:13 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (77.42%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    vector<pair<int,int>>dirc={{-1,0},{0,1},{1,0},{0,-1}};
-    void f(int r,int c,int color,int iniColor,vector<vector<int>>& image,int n,int m){
-
+    vector<pair<int,int>>dirc={{1,0},{0,1},{-1,0},{0,-1}};
+    void f(int i,int j,vector<vector<int>>& image,int n,int m,int color,int prevColor){
+        image[i][j]=color;
         for(auto it:dirc){
-            int row =r+it.first;
-            int col=c+it.second;
-            if(row>=0 && row<n && col>=0 && col<m && image[row][col]==iniColor){
-                image[row][col]=color;
-                f(row,col,color,iniColor,image,n,m);
+            int r=i+it.first;
+            int c=j+it.second;
+            if(r>=0 && r<n && c>=0 && c<m && image[r][c]==prevColor){
+                f(r,c,image,n,m,color,prevColor);
             }
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n=image.size();
         int m=image[0].size();
-        if(image[sr][sc]==color)return image;
-        f(sr,sc,color,image[sr][sc],image,n,m);
-        image[sr][sc]=color;
+        if(color==image[sr][sc])return image;
+        f(sr,sc,image,n,m,color,image[sr][sc]);
         return image;
-
     }
 };
