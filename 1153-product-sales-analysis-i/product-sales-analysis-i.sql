410eb0ab19d41d109df6bb52b46afd35f9ed24f8
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-07-30 19:40:18 +0530

    Time: 1348 ms (32.93%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 -- right join -> means right table first
 -- just main the order of table
-select product_name, year , price from Product right join 
+select product_name, year , price from Product  join 
 Sales on Sales.product_id=Product.product_id;
