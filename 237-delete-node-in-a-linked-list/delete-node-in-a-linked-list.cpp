656ea64e5af31a6c972dd42bcf1f35e848e7e69d
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-08-02 12:32:28 +0530

    Time: 5 ms (77.21%) | Memory: 12.3 MB (53.52%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -10,18 +10,20 @@ class Solution {
 public:
     void deleteNode(ListNode* node) {
 
-        ListNode* temp=node->next;
-        if(!temp){node=NULL;
-        return;}
-        node->val=temp->val;
-        node->next=temp->next;
+        // ListNode* temp=node->next;
+        // if(!temp){node=NULL;
+        // return;}
+        // node->val=temp->val;
+        // node->next=temp->next;
 
-    //    ListNode* temp=node->next;
-    //    while(temp){
-    //     node->val=temp->val;
-    //     node=temp;
-    //     temp=temp->next;
-    //    }
-    //    node=NULL;
+       ListNode* temp=node->next;
+       while(temp){
+        node->val=temp->val;
+        if(!temp->next){
+            node->next=NULL;
+        }
+        else node=temp;
+        temp=temp->next;
+       }
     }
 };
