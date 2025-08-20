91a7b142561a0af3d19b2bbabe6d37ac477786eb
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-08-20 11:26:35 +0530

    Time: 1297 ms (78.30%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,11 +1,16 @@
 # Write your MySQL query statement below
--- select * from SalesPerson s left join Orders o 
--- on s.sales_id =o.sales_id join Company c on
--- o.com_id =c.com_id
 SELECT s.name
-FROM Salesperson s
-WHERE s.sales_id NOT IN(
-    select o.sales_id from Orders o where o.com_id=(
-        select c.com_id from Company c where c.name='RED'
-    )
-)
+FROM SalesPerson s
+WHERE s.sales_id NOT IN (
+    SELECT o.sales_id
+    FROM Orders o
+    JOIN Company c ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+);
+-- SELECT s.name
+-- FROM Salesperson s
+-- WHERE s.sales_id NOT IN(
+--     select o.sales_id from Orders o where o.com_id=(
+--         select c.com_id from Company c where c.name='RED'
+--     )
+-- )
