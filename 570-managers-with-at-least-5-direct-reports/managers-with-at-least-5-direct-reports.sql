ad46c44ca20c380f682c77807a963614520e3fa9
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-08-21 17:29:51 +0530

    Time: 380 ms (26.08%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select e1.name from Employee as e1 left join Employee as e2
-on e1.id=e2.managerId group by e2.managerId having count(e2.managerId)>4;
+select e1.name from Employee as e1 join Employee as e2
+on e1.id=e2.managerId group by e1.id having count(e2.managerId)>4;
