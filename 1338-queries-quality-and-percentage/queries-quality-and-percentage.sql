ddad877b05638238be1015b931d3ef30b085d7d5
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-08-21 19:18:59 +0530

    Time: 410 ms (18.97%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
+-- (select count(*) from Queries where rating <3 group by query_name)
+-- This returns multiple rows because GROUP BY query_name is in the subquery.
 select query_name,round(avg(rating/position),2) as quality
 ,round(sum(case when rating <3 then 1 else 0 end)/count(*)* 100.0, 2) as poor_query_percentage 
 from Queries group by query_name;
