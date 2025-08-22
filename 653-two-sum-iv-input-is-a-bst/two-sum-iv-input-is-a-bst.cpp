0473f250d4e5277ce373242b6167428fc8bb6a99
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-08-22 23:33:13 +0530

    Time: 7 ms (28.79%) | Memory: 38.8 MB (26.52%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -6,9 +6,6 @@ public:
         if (!root) return false;
         if (set.count(k - root->val)) return true;
         set.insert(root->val);
-        int l=0,r=0;
-        if(findTarget(root->left, k))return l=1;;
-        if(findTarget(root->right, k))return r=1;;
-        return l|r;
+        return findTarget(root->left, k) || findTarget(root->right, k);
     }
 };
