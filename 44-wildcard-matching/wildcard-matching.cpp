705b6ebe198b46f6ee7c9cee40834e0c1107d4fd
harshraj099 <157514173+harshraj099@users.noreply.github.com>
2025-08-28 18:01:41 +0530

    Time: 36 ms (32.74%) | Memory: 31.5 MB (29.64%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -17,36 +17,36 @@ public:
        int take=0,not_take=0;
         if(s[i]==p[j] | p[j]=='?')take=f(i+1,j+1,n,m,s,p,dp);
         else if(p[j]=='*') not_take=f(i+1,j,n,m,s,p,dp)|f(i,j+1,n,m,s,p,dp);  //some characters or zero characters
-        else return 0;
+        // else return 0;
 
         return  dp[i][j]=take|not_take;
     }
     bool isMatch(string s, string p) {
         int n=s.size();
        int m=p.size();;
-        // vector<vector<int>>dp(n,vector<int>(m,-1));
-        // return f(0,0,n,m,s,p,dp);
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return f(0,0,n,m,s,p,dp);
 
          // tabulation
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        // vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         // base
-        dp[0][0]=1;
-        for(int j=1;j<=m;j++){
-       if(p[j-1]=='*')dp[0][j]=1;
-          else  break;   
-        }
+    //     dp[0][0]=1;
+    //     for(int j=1;j<=m;j++){
+    //    if(p[j-1]=='*')dp[0][j]=1;
+    //       else  break;   
+    //     }
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-           int take=0,not_take=0;
-        if(s[i-1]==p[j-1] | p[j-1]=='?')take=dp[i-1][j-1];
-        else if(p[j-1]=='*') not_take=dp[i-1][j]|dp[i][j-1];
-        // else dp[i][j]=0;
+    //     for(int i=1;i<=n;i++){
+    //         for(int j=1;j<=m;j++){
+    //        int take=0,not_take=0;
+    //     if(s[i-1]==p[j-1] | p[j-1]=='?')take=dp[i-1][j-1];
+    //     else if(p[j-1]=='*') not_take=dp[i-1][j]|dp[i][j-1];
+    //     // else dp[i][j]=0;
 
-         dp[i][j]=take|not_take;
-            }
-        }
-        return dp[n][m];
+    //      dp[i][j]=take|not_take;
+    //         }
+    //     }
+    //     return dp[n][m];
 
     }
 };
